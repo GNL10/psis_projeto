6b67acb391781ebaf50b1dd864bce947c42640f4
gnl <goncalo_mao_cheia@hotmail.com>
2019-05-01 03:00:40 +0100

    Bot functional

--- memory_multiple/bot.c
+++ memory_multiple/bot.c
@@ -6,77 +6,149 @@
 #include "connections.h"
 
 int done = 0;	// Variable used to know when the game ends
-board_place **board_known;	// board that stores all the cards that have been shown
+board_place *board_known;	// board that stores all the cards that have been shown
+int dim;
 
-void *thread_update_board (void *arg) {
-	play_response resp;
 
-	while (!done) {
-		recv(sock_fd, &resp, sizeof(resp), 0);
-		switch (resp.code) {
-						case 1:	// First play -> write down the first card
-							strcpy(board_known[resp.play1[0]][resp.play1[1]].v, resp.str_play1);
-							break;
-						case 3:	// End of the game
-						  done = 1;
-						case 2:	// Second play
-							// Assign a default string to taken places
-							strcpy(board_known[resp.play1[0]][resp.play1[1]].v, "\0");
-							strcpy(board_known[resp.play2[0]][resp.play2[1]].v, "\0");
-							break;
-						case -2: // Wrong play -> write down the second card
-							strcpy(board_known[resp.play2[0]][resp.play2[1]].v, resp.str_play2);
-							break;
-					}
-	}
-	return NULL;	// To ignore the warning
-}
-
-board_place ** init_bot_board (int dim) {
-	int x, y;
-	board_place **board;
-
-	board = (board_place**)malloc(dim*sizeof(board_place*));
-	for (x = 0; x < dim; x++) {
-		printf("TESTE\n");
-		board_known[x] = (board_place*) malloc(sizeof(board_place));
-	}
-
-	// reset the board
-	for (x = 0; x < dim; x++)
-		for (y = 0; y < dim; y++)
-			strcpy (board_known[x][y].v, "HI");
-
-	return board;
-}
-
-void print_bot_board (int dim) {
-	int x, y;
-	for (x = 0; x < dim; x++) {
-		for (y = 0; y < dim; y++)
-			printf("%s\n", board_known[x][y].v);
-		printf("\n");
-	}
-}
+int lin_conv (int x, int y);
+board_place * init_bot_board ();
+void print_bot_board ();
+void *thread_update_board (void *arg);
+int find_pair (int *x1, int *y1, int *x2, int *y2);
+void random_play (int *x, int *y);
 
 
 int main(int argc, char const *argv[]) {
 	struct sockaddr_in server_addr;
 	struct sockaddr_in addr;
 	pthread_t thread_id;
-	int dim = 4; // In the future this will be received from the server
+	int x1, y1;
+	int x2, y2;
 
-	//establish_client_connections(&server_addr, &addr);
-	board_known = init_bot_board(dim);
-	print_bot_board(dim);
+	srand(123); // TO CHANGE !!!!
+	establish_client_connections(&server_addr, &addr);
+	dim = 4; // In the future this will be received from the server
+	board_known = init_bot_board();
 	// Create thread that will receive and continuously update matrix
-	/*pthread_create(&thread_id, NULL, thread_update_board, NULL);
-	
-	while (!done){
-		int board_x= 0, board_y = 0;
-		send(sock_fd, &board_x, sizeof(board_x), 0);
-		send(sock_fd, &board_y, sizeof(board_y), 0);
-	}*/
+	pthread_create(&thread_id, NULL, thread_update_board, NULL);
+
+	while (1){
+		if (find_pair(&x1, &y1, &x2, &y2) == 1) {
+			printf("AVAILABLE PAIR in (%d - %d) and (%d - %d)!!!\n", y1, x1, y2, x2 );
+			send(sock_fd, &x1, sizeof(x1), 0);
+			send(sock_fd, &y1, sizeof(y1), 0);
+			send(sock_fd, &x2, sizeof(x2), 0);
+			send(sock_fd, &y2, sizeof(y2), 0);
+		}
+		else {
+			random_play(&x1, &y1);
+			printf("Bot played : %d - %d -> %s\n", y1, x1, board_known[lin_conv(x1,y1)].v);
+			send(sock_fd, &x1, sizeof(x1), 0);
+			send(sock_fd, &y1, sizeof(y1), 0);
+
+			random_play(&x2, &y1);
+			printf("Bot played : %d - %d -> %s\n", y2, x2, board_known[lin_conv(x2,y2)].v);
+			send(sock_fd, &x2, sizeof(x2), 0);
+			send(sock_fd, &y2, sizeof(y2), 0);
+		}
+		sleep(4);
+	}
 
 	return 0;
+}
+
+
+int lin_conv (int x, int y) {
+	return y*dim+x;
+}
+
+/*	init_bot_board
+	Allocates memory for the bot's auxiliar board and initializes every position to "\0"
+*/
+board_place * init_bot_board () {
+	int i;
+	board_place *board;
+
+	board = (board_place*) malloc(sizeof(board_place)* dim *dim);
+
+	// reset the board
+	for( i=0; i < (dim*dim); i++){
+		strcpy(board[i].v, "\0");
+  	}
+	return board;
+}
+
+void print_bot_board () {
+	int x, y;
+
+	printf("Printing updated board\n\n");
+	for (x = 0; x < dim; x++) {
+		for (y = 0; y < dim; y++)
+			printf("%s ", board_known[lin_conv(y, x)].v);
+		printf("\n");
+	}
+}
+
+/*	thread_update_board	
+	This function is implemented in a thread, and throughout the game it receives and updates the
+	bot's auxiliar board
+*/
+void *thread_update_board (void *arg) {
+	play_response resp;
+
+	while (!done) {
+		recv(sock_fd, &resp, sizeof(resp), 0);
+		printf("Bot received: %d - %d with string %s\n", resp.play1[1], resp.play1[0], resp.str_play1);
+		printf("       play2: %d - %d with string %s\n", resp.play2[1], resp.play2[0], resp.str_play2);
+		printf("Resp code = %d\n", resp.code);
+		switch (resp.code) {
+			case 1:	// First play -> write down the first card
+				strcpy(board_known[lin_conv(resp.play1[0], resp.play1[1])].v, resp.str_play1);
+				break;
+			case 3:	// End of the game
+			 	done = 1;
+			case 2:	// Second play
+				// Assign a default string to taken places
+				strcpy(board_known[lin_conv(resp.play1[0], resp.play1[1])].v, "--");
+				strcpy(board_known[lin_conv(resp.play2[0], resp.play2[1])].v, "--");
+				break;
+			case -2: // Wrong play -> write down the second card
+				strcpy(board_known[lin_conv(resp.play2[0], resp.play2[1])].v, resp.str_play2);
+				break;
+		}
+		print_bot_board();
+	}
+	return NULL;	// To ignore the warning
+}
+
+int find_pair (int *x1, int *y1, int *x2, int *y2) {
+	int i1, i2;
+
+	// dim*dim-1 because if i1 is the last element, then it has no pair
+	for (i1 = 0; i1 < dim*dim-1; i1++) {
+		// if the position is neither a \0 nor a -- then it is a string	
+		if ((strcmp(board_known[i1].v, "\0") != 0) && (strcmp(board_known[i1].v, "--") != 0)) {		
+			for (i2 = i1+1; i2 < dim*dim; i2++) {
+				if (strcmp(board_known[i1].v, board_known[i2].v) == 0){
+					*x1 = i1 % dim;
+					*y1 = i1 / dim;
+					*x2 = i2 % dim;
+					*y2 = i2 / dim;
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+/*	random_play
+	makes a random play, avoiding the cards that have been matched
+*/
+void random_play (int *x, int *y) {
+		do {
+			*x = rand()%dim;
+		 	*y = rand()%dim;
+		 	// IMPORTANT: while playing in multiplayer, if there are no cards left this will do an infinite loop
+		} while (strcmp(board_known[lin_conv(*x,*y)].v, "\0") != 0);
 }
--- memory_multiple/memory_server.c
+++ memory_multiple/memory_server.c
@@ -15,18 +15,20 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in address;
     int client;
     int board_x, board_y;
-
+    int done = 0;
     establish_server_connections(&address, &server_fd);
 
     client = server_accept_client(&address, &server_fd);
     
     init_board(4);
 
-    while (1) {
+    while (!done) {
         recv(client, &board_x, sizeof(board_x), 0);
         recv(client, &board_y, sizeof(board_y), 0);
         play_response resp = board_play(board_x, board_y);
         send(client, &resp, sizeof(resp), 0);
+        if(resp.code == 3)  // End of the game
+            done = 1;
     }
 
     close(server_fd);
