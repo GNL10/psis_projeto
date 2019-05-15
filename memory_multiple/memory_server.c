032321569b6cda96220ceb2e46d9e969b998d02b
gnl <goncalo_mao_cheia@hotmail.com>
2019-05-15 14:21:57 +0100

    MERGING

--- memory_multiple/memory_server.c
+++ memory_multiple/memory_server.c
@@ -23,12 +23,12 @@ void Update_Board (card_info *card, int x, int y, int c_color[3], char* str, int
 void send_all_clients (card_info card);
 Node * Add_Client (int new_client);
 Node * Remove_Client (int client);
-void Allocate_Mutexes_Array (int dim, pthread_mutex_t** mux_a);
+void Allocate_Mutexes_Array (int dim, pthread_mutex_t*** mux_a);
 card_info* Allocate_Board_Cards (int dim);
 void Send_Board (int socket, card_info* board, int dim);
 int Convert_Coordinates (int x, int y, int dim);
 
-pthread_mutex_t **mutexes;
+pthread_mutex_t** mutexes;
 
 Node * Client_list;
 
@@ -44,11 +44,17 @@ int main(int argc, char const *argv[]) {
 
     //Board_size = atoi (argv[1]);
     Board_size = 4;
-    Allocate_Mutexes_Array (Board_size, mutexes);
+    Allocate_Mutexes_Array (Board_size, &mutexes);
     Board_cards = Allocate_Board_Cards (Board_size);
 
+/*    
+    pthread_mutex_lock(&mutexes[1][2]); // trylock return 0 se conseguir dar lock, erro se nao conseguir dar lock
+    printf("TRYLOCK %d\n", pthread_mutex_trylock(&mutexes[1][2]));
+    pthread_mutex_unlock(&mutexes[1][2]);
+    printf("TRYLOCK %d\n", pthread_mutex_trylock(&mutexes[1][2]));
+*/
     establish_server_connections(&address, &server_fd);
-    init_board(4);
+    init_board(Board_size);
 
     while(1){
         Client_list = Add_Client(server_accept_client(&address, &server_fd));
@@ -124,7 +130,7 @@ void* connection_thread (void* socket_desc){
                 Update_Board(&Board_cards[j], resp.play2[0], resp.play2[1], white, NULL, black, 0);
                 send_all_clients(Board_cards[j]);
                 break;
-            case -1:
+            case -1:    // Turn the card back down (to be erased)
                 set_card_state(resp.play1[0], resp.play1[1], DOWN);
                 Update_Board(&Board_cards[i], resp.play1[0], resp.play1[1], white, NULL, black, 0);
                 send_all_clients(Board_cards[i]);
@@ -153,6 +159,7 @@ void* first_card_timeout (void* arg) {
         }
     }
     if (card_changed == 0) {
+        printf("Card changed set down the card\n");
         set_card_state(coords[0], coords[1], DOWN);
         Update_Board(&card, coords[0], coords[1], white, NULL, black, 0);
         send_all_clients(card);
@@ -233,13 +240,13 @@ Node * Remove_Client (int client){
     return Client_list;
 }
 
-void Allocate_Mutexes_Array (int dim, pthread_mutex_t** mux_a){
+void Allocate_Mutexes_Array (int dim, pthread_mutex_t*** mux_a){
 
-    mux_a = malloc (sizeof(pthread_mutex_t*)*dim);
+    *mux_a = malloc (sizeof(pthread_mutex_t*)*dim);
 
-    for (int i = 0; i < dim; i++)
+    for (int x = 0; x < dim; x++)
     {
-        mux_a[i] = malloc (sizeof (pthread_mutex_t));
+        (*mux_a)[x] = malloc (sizeof (pthread_mutex_t)*dim);
     }
 }
 
