f59599f0d944a83fd003cabcb486dadd79b1ac47
Laisa <laisa0420@gmial.com>
2019-05-30 22:00:33 +0100

    c

--- memory_multiple/memory_server.c
+++ memory_multiple/memory_server.c
@@ -21,7 +21,6 @@ int white[3] = {255,255,255}, black[3] = {0,0,0}, red[3] = {255,0,0}, grey[3]={2
 // mudar os mutexes para a a board em si, deve ser melhor
 void* connection_thread (void* socket_desc);
 void Update_Board (board_place *card, int c_color[3], int s_color[3]);
-card_info* Allocate_Board_Cards (int dim);
 int read_arguments (int argc, char* argv);
 void Send_Board (int socket, board_place* board, int dim);
 void Copy_Card (board_place board, card_info* card, int board_x, int board_y);
@@ -45,7 +44,7 @@ int main(int argc, char const *argv[]) {
 
     while(1){
         Client_list = Add_Client(server_accept_client(&address, &server_fd));
-        pthread_create (&thread_id[i], NULL, connection_thread, (void*)&Client_list->client.client_socket);
+        pthread_create (&thread_id[i], NULL, connection_thread, (void*)Client_list);
         i++;
     }
 
@@ -58,7 +57,8 @@ int main(int argc, char const *argv[]) {
 void* connection_thread (void* socket_desc){
     int board_x, board_y;
     play_response resp;
-    int client_socket = *(int*)socket_desc;
+    Node * current_client = (Node*)socket_desc;
+    //int client_socket = *(int*)socket_desc;
     int play1[2] = {-1, 0};
     int recv_size;
     int i,j;
@@ -70,13 +70,13 @@ void* connection_thread (void* socket_desc){
     int player_color[3]={faded_player_color[0]+50,faded_player_color[1],faded_player_color[2]};
 
     //Send current board game when client connects for the first time
-    Send_Board (client_socket, board, Board_size);
+    Send_Board (current_client->client.client_socket, board, Board_size);
 
     while(endgame != 1){
-        recv_size = recv(client_socket, &board_x, sizeof(board_x), 0);
+        recv_size = recv(current_client->client.client_socket, &board_x, sizeof(board_x), 0);
         if (recv_size == 0)
             break;
-        recv_size = recv(client_socket, &board_y, sizeof(board_y), 0);
+        recv_size = recv(current_client->client.client_socket, &board_y, sizeof(board_y), 0);
         if (recv_size == 0)
             break;  
 
@@ -97,7 +97,6 @@ void* connection_thread (void* socket_desc){
                     Update_Board(board, white, black);
                     Copy_Card (*board, &card, resp.play1[0], resp.play1[1]);
                     send_all_clients(card);
-                    resp.code = -2;
                 }
                 break;
             case 3: // end of game
@@ -143,7 +142,7 @@ void* connection_thread (void* socket_desc){
                 break;
         }
     }
-    Remove_Client(client_socket); 
+    Remove_Client(current_client->client.client_socket); 
     printf("Closing connection_thread\n");
     return 0;
 }
@@ -161,28 +160,6 @@ void Update_Board (board_place *board, int c_color[3], int s_color[3]) {
         board->string_color[2] = s_color[2];
     //pthread_mutex_unlock (&board->mutex);
 }   
-
-card_info* Allocate_Board_Cards (int dim){
-
-    int i;
-
-    card_info* array = malloc (sizeof(card_info)*dim*dim);
-
-    for (i=0; i < dim*dim; i++){
-        array[i].x = -1;
-        array[i].y = -1;
-        array[i].card_color[0] = 255;
-        array[i].card_color[1] = 255;
-        array[i].card_color[2] = 255;
-        strcpy (array[i].string, "\0");
-        array[i].string_color[0] = 255;
-        array[i].string_color[1] = 255;
-        array[i].string_color[2] = 255;
-        array[i].end = 0;
-        array[i].state = 0;
-    }
-    return array;
-}
 
 int read_arguments (int argc, char*argv) {
     int size = 0;
@@ -262,14 +239,13 @@ int Count_5_seconds (){
     int timeout;
     int fd = 0;
     int ret;
-    card_info card;
 
     while(1){
         fds[0].fd = fd;
         fds[0].events = 0;
         fds[0].events |= POLLIN;
 
-        timeout = 1000;
+        timeout = 5000;
         ret = poll(fds,1,timeout);
         return ret;
     }
